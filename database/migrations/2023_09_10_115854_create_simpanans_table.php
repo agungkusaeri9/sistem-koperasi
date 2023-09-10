05a88bf63dad6b0595fc7adcb0b2f0b66d734ee0
Agung Kusaeri <agung.kusaeri9@gmail.com>
2023-09-11 00:39:01 +0700

    feat(simpanan) - Setup fitur simpanan

---
+++ database/migrations/2023_09_10_115854_create_simpanans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('simpanan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('jenis_simpanan_id')->constrained('jenis_simpanan');
+            $table->integer('nominal');
+            $table->foreignId('anggota_id')->constrained('anggota')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('metode_pembayaran')->constrained('metode_pembayaran');
+            $table->boolean('status_pencairan')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('simpanan');
+    }
+};
