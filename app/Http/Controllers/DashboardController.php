20703f4cd149e6812d94cb354b64cbb1175c744f
Agung Kusaeri <agung.kusaeri9@gmail.com>
2023-09-13 18:04:42 +0700

    feat(dashboard) - Membuat fitur dashboard admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Anggota;
+use App\Models\Pinjaman;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,8 +12,17 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $admin_count = [
+            'jumlah_anggota' => Anggota::count(),
+            'jumlah_pinjaman' => Pinjaman::count(),
+            'jumlah_pinjaman_menunggu_persetujuan' => Pinjaman::where('status', 0)->count(),
+            'jumlah_pinjaman_selesai' => Pinjaman::where('status', 2)->count()
+        ];
+        $pinjaman_terakhir = Pinjaman::with(['anggota'])->latest()->limit(10)->get();
         return view('pages.dashboard', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'admin_count' => $admin_count,
+            'pinjaman_terakhir' => $pinjaman_terakhir
         ]);
     }
 }
